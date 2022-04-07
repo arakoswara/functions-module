13e76f631cc62046993d4e7cd577f3895c9ee47e
arakoswara <arakoswara2@gmial.com>
2022-04-07 13:41:02 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arakoswara/functions-module
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,7 @@
+package functions_module
+
+import "strings"
+
+func UcWords(str string) string {
+	return strings.ToTitle(str)
+}
