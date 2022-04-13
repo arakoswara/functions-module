0469e3b02e5dc419a1ad48b008dcd29b818b6a68
arakoswara <arakoswara2@gmial.com>
2022-04-14 05:17:30 +0700

    add function random string

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package functions_module
 
-import "strings"
+import (
+	"math/rand"
+	"strings"
+)
 
 func UcWords(str string) string {
 	return strings.ToTitle(str)
+}
+
+func GenerateString(total int) string {
+	var letterRunes = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	b := make([]rune, total)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
